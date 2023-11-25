0999e2a0f586dbb8e2a4eb01aaf7ca087d419372
Mojtaba Izadi <mjt.izadi@gmail.com>
2023-11-25 08:53:15 -0700

    added reorder for half-letter print

---
+++ reorder for half-letter print.py
@@ -0,0 +1,33 @@
+import numpy as np
+from pypdf import PdfReader, PdfWriter
+
+
+def find_new_order(n_pages):
+    len_new = np.int8(np.ceil(n_pages / 4)) * 4
+    new_order = np.reshape(np.array([[4 * j + 1, 4 * j + 3,
+                                      4 * j + 4, 4 * j + 2]
+                                      for j in range(len_new // 4)]),
+                           (len_new,))
+    new_order[new_order > n_pages] = 0
+    return(new_order)
+
+file_path = "/Users/Mojtaba/Desktop/"
+file_name = "cvy305.pdf"
+file = file_path + file_name
+reordered_file = file[:-4] + "_reordered.pdf"
+
+pdf_reader = PdfReader(file)
+pdf_writer = PdfWriter()
+
+total_pages = len(pdf_reader.pages)
+new_order = find_new_order(total_pages)
+
+for page in new_order:
+    print(page)
+    if page == 0:
+        pdf_writer.add_blank_page()
+    else:
+        pdf_writer.add_page(pdf_reader.pages[int(page - 1)])
+
+with open(reordered_file, 'wb') as fh:
+    pdf_writer.write(fh)
